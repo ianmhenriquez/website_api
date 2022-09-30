4e809027fc479a8bbedb7804697c2ab9d77ac89b
ianmhenriquez <ianmhenriquez@hotmail.com>
2022-09-29 21:03:06 -0400

    changes api msg

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-    res.send("I love you danique");
+    res.send("Hi danique and mom and dad");
 });
 
 module.exports = router;
